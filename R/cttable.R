6cf6e3549d5d03c4e3e8bb504b97726f54f26d6b
Michael Lerch <mdlerch@gmail.com>
2014-12-18 13:10:30 -0700

    Move print.cttable to cttable

--- R/cttable.R
+++ R/cttable.R
@@ -1,29 +1,45 @@
-#'
-#' \code{cttable} Build a contingency table or set of contingency tables out
-#' of an input vector or matrix
-#'
-#' @param input numeric numeric vector of length 4n where n is an integer
-
-#'
-#' @return numeric matrix or array of class \code{cttable} depending on size of n
-#' @export
-#'
-
-# page 60 table 6.1 a b c d is DE nDE DnE nDnE
+# page 60 table 6.1 a b c d is DE dE De de
 cttable <- function(input)
 {
-	input <- c(input)
-	if (length(input) %% 4 == 0)
-	{
-		out <- array(input, dim = c(2, 2, nf <- length(input) / 4),
-		             dimnames = list(case=c("D", "!D"), exposure = c("E", "!E"),
-		                             factors = paste("f", 1:nf, sep = "")))
-		out <- aperm(out, c(2, 1, 3))
-		class(out) <- "cttable"
-		attr(out, "factors") <- nf
-	} else {
-		stop("incorrect size")
-	}
-	return(out)
+    if (class(input) == "cttable")
+    {
+        return(cttable)
+    }
+    input <- c(input)
+    if (length(input) %% 4 == 0)
+    {
+        out <- array(input, dim = c(2, 2, nf <- length(input) / 4),
+                     dimnames = list(case=c("D", "!D"), exposure = c("E", "!E"),
+                                     factors = paste("f", 1:nf, sep = "")))
+        out <- aperm(out, c(2, 1, 3))
+        class(out) <- "cttable"
+        attr(out, "factors") <- nf
+    } else {
+        stop("incorrect size")
+    }
+    return(out)
+}
+
+print.cttable <- function(cttable)
+{
+    if (class(cttable) != "cttable")
+    {
+        stop("This is not a cttable")
+    }
+    nf <- attr(cttable, "factors")
+
+    if (nf == 1)
+    {
+        print(as.table(cttable[ ,  , 1]))
+    } else {
+        for (i in 1:nf)
+        {
+            msg <- paste("Factor ", i, "\n", sep = "")
+            cat(msg)
+            print(as.table(cttable[ , , i]))
+            cat("\n")
+        }
+    }
+    invisible(cttable)
 }
 
